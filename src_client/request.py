ea27be1189f1ce602688f0fd2a7ff50f3bf38b4c
m_hikichi <hijiki.mshr19980119@outlook.com>
2023-08-09 20:57:49 +0900

    chore: API仕様書を自動で作成させるために，FastAPIで書き換え

--- src_client/request.py
+++ src_client/request.py
@@ -6,8 +6,8 @@ if __name__=="__main__":
     url = "http://whisper_server:5000/whisper"
     with open("/app/voice_recordings/voicevox.wav", "rb") as f:
         audio = f.read()
-        b64_audio = base64.b64encode(audio)
+        b64_audio = base64.b64encode(audio).decode("utf-8")
 
-    files = {"b64_audio": b64_audio}
-    response = requests.post(url, files=files)
+    data = {"b64_audio": b64_audio}
+    response = requests.post(url, json=data)
     print(response.text)
--- src_server/main.py
+++ src_server/main.py
@@ -1,35 +1,50 @@
-from flask import Flask, abort, request
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import HTMLResponse
+from pydantic import  BaseModel
 import whisper
 import tempfile
 from pathlib import Path
 import base64
 
 
-app = Flask(__name__)
+app = FastAPI(
+    title="whisper API",
+    description="",
+)
 
-@app.route("/")
+
+class RequestAudioModel(BaseModel):
+    b64_audio: str
+
+
+class TranscribeTextModel(BaseModel):
+    transcribe_text: str
+
+
+@app.get(
+    "/",
+    description="ホームページの表示"
+)
 def index():
-    """
-    ホームページの表示
-    """
     display_text = """
     <h1>Welcome to the Whisper API!</h1>
     """
-    return display_text
+    return HTMLResponse(content=display_text)
 
 
-@app.route("/whisper", methods=["POST"])
-def whisper_handler():
-    """
-    whisperのリクエストハンドリグ
-    """
-    if not request.files:
-        # リクエストにファイルが含まれていない場合は, HTTP Exception 400を返す
-        abort(400)
+@app.post(
+    "/whisper",
+    response_model=TranscribeTextModel,
+    description="base64形式の音声ファイルを受け取り, 文字起こしした文章を返す"
+)
+def whisper_handler(request_audio: RequestAudioModel):
+    if not request_audio.b64_audio:
+        # return HTTP Exception 400 if b64_audio is blank
+        raise HTTPException(status_code=400, detail="audio file is None")
 
     # temporary storage of received audio file
-    b64_audio = request.files["b64_audio"]
-    audio = base64.b64decode(b64_audio.read())
+    b64_audio = request_audio.b64_audio
+    audio = base64.b64decode(b64_audio)
     with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as temp_file:
         temp_filepath = Path(temp_file.name)
     with open(str(temp_filepath), "wb") as f:
@@ -44,8 +59,5 @@ def whisper_handler():
     # delete temporary file
     temp_filepath.unlink()
 
-    return result["text"]
-
-
-if __name__=="__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # return JSONResponse(content={"transcribe_text": result["text"]})
+    return TranscribeTextModel(transcribe_text=result["text"])
