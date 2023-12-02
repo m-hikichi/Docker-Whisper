3b00b57bed9aa83b963b3a90af38e31845b96216
m_hikichi <hijiki.mshr19980119@outlook.com>
2023-12-02 11:53:40 +0900

    chore: 精度のよいモデルで推論を行えるようにするために，faster whisper large-v3をdocker imageに含めるように修正

--- Dockerfile_faster_whisper/download_model.py
+++ Dockerfile_faster_whisper/download_model.py
@@ -1,5 +1,39 @@
-import faster_whisper
+import argparse
+from faster_whisper import WhisperModel
 
 
-for model_name in ["tiny", "base", "small", "medium", "large-v1", "large-v2"]:
-    _ = faster_whisper.WhisperModel(model_name, device="cpu", compute_type="int8")
+def download_model(model_name: str, device: str = "cpu", compute_type: str = "int8") -> WhisperModel:
+    """
+    指定されたモデルをダウンロード.
+
+    Parameters:
+        model_name (str): ダウンロードするモデルの名前.
+        device (str, optional): モデルのデプロイ先デバイス. デフォルトは "cpu".
+        compute_type (str, optional): モデルの計算タイプ. デフォルトは "int8".
+
+    Returns:
+        WhisperModel: ダウンロードされたWhisperモデル.
+    """
+    whisper_model = WhisperModel(model_name, device=device, compute_type=compute_type)
+    return whisper_model
+
+
+def parse_arguments() -> argparse.Namespace:
+    """
+    コマンドライン引数を解析.
+
+    Returns:
+        argparse.Namespace: 解析されたコマンドライン引数.
+    """
+    parser = argparse.ArgumentParser(description="Download the specified model.")
+    parser.add_argument("model_name", help="Specify the model to be downloaded.")
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+    model = download_model(args.model_name, device="cpu", compute_type="int8")
+
+
+if __name__ == "__main__":
+    main()
